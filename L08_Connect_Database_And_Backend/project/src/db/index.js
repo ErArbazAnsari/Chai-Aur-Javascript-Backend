8300b7ab27bcab007a5fd2da72e320316577d757
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-10-22 21:07:31 +0530

    connectivity b/t backend and database mongodb

---
+++ L08_Connect_Database_And_Backend/project/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connnectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+    console.log("Data is connected successfully!");
+    console.log("Connection Instance : ", connectionInstance);
+  } catch (error) {
+    console.log("Error Found: ", error);
+    process.exit(1);
+  }
+};
+
+export default connnectDB;
